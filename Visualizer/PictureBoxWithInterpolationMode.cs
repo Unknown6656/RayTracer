72b9aff58154b5c6ce0c93a7e5a2f490c0589b3c
unknown6656 <unknown84@gmx.de>
2021-01-09 15:21:49 +0100

    added NN-picturebox

---
+++ Visualizer/PictureBoxWithInterpolationMode.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Visualizer
+{
+    public class PictureBoxWithInterpolationMode
+        : PictureBox
+    {
+        public InterpolationMode InterpolationMode { get; set; }
+
+
+        public PictureBoxWithInterpolationMode() => DoubleBuffered = true;
+
+        protected override void OnPaint(PaintEventArgs paintEventArgs)
+        {
+            paintEventArgs.Graphics.InterpolationMode = InterpolationMode;
+
+            base.OnPaint(paintEventArgs);
+        }
+    }
+}
