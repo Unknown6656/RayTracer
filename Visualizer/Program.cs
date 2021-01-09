6caac7e09c96686a1a077b63d37b9bd075dd169a
unknown6656 <unknown84@gmx.de>
2021-01-09 15:21:57 +0100

    added visualizer program

---
+++ Visualizer/Program.cs
@@ -0,0 +1,16 @@
+using System.Windows.Forms;
+using System.Threading;
+
+using Visualizer;
+
+
+Thread thread = new Thread(() =>
+{
+    Application.SetHighDpiMode(HighDpiMode.SystemAware);
+    Application.EnableVisualStyles();
+    Application.SetCompatibleTextRenderingDefault(false);
+    Application.Run(new MainWindow());
+});
+thread.SetApartmentState(ApartmentState.STA);
+thread.Start();
+thread.Join();
---
+++ Visualizer/Wrapper.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.InteropServices;
+
+
+namespace Visualizer
+{
+    public enum RenderMode
+    {
+        Colors,
+        Depths,
+        SurfaceNormals,
+        Iterations,
+        RenderTime,
+    }
+
+    public struct Vec3
+    {
+        public double X, Y, Z;
+
+        public Vec3(double x, double y, double z) : this() => (X, Y, Z) = (x, y, z);
+    }
+
+    public struct CameraConfiguration
+    {
+        public Vec3 Position;
+        public Vec3 LookAt;
+        public double FOV;
+        public double FocalLength;
+    }
+
+    //[StructLayout(LayoutKind.Sequential)]
+    public struct RenderConfiguration
+    {
+        public ulong HorizontalResolution;
+        public ulong VerticalResolution;
+        public ulong SubpixelsPerPixel;
+        public ulong SamplesPerSubpixel;
+        public ulong MaximumIterationCount;
+        public CameraConfiguration Camera;
+        public RenderMode RenderMode;
+    };
+
+    public static class RayTracer
+    {
+        [DllImport("RayTracer.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static unsafe extern void RenderImage(RenderConfiguration config, (double A, double R, double G, double B)* buffer);
+    }
+}
