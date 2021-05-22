95bd795dc63bfb351ee4a34008d66903556d5d2b
unknown6656 <unknown84@gmx.de>
2021-05-22 14:13:49 +0200

    added more common includes and functions

--- RayTracer/common.hpp
+++ RayTracer/common.hpp
@@ -6,9 +6,15 @@
 #include <string>
 #include <ostream>
 #include <algorithm>
-#include <vector>
 #include <tuple>
 #include <stdexcept>
+#include <assert.h>
+#include <stdio.h>
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <ctime>
+#include <ppl.h>
 
 
 #define EPSILON 1e-6
@@ -16,6 +22,13 @@
 
 #define OSTREAM_OPERATOR(x) friend inline std::ostream& operator<<(std::ostream& os, const x& value) { os << value.to_string(); return os; }
 #define CPP_IS_FUCKING_RETARDED(x) inline x& operator =(const x& value) noexcept { return this == &value ? *this : *new(this)x(value); }
+
+#define ABSTRACT(ret, name, ...) \
+    virtual ret name(__VA_ARGS__) const = 0 \
+    { \
+        std::cerr << "Using the abstract function '" #ret " " #name "(" #__VA_ARGS__ ")'." << std::endl; \
+        return ret(); \
+    }
 
 
 constexpr bool solve_quadratic(const float& a, const float& b, const float& c, float* __restrict const x0, float* __restrict const x1)
@@ -44,3 +57,25 @@ constexpr bool solve_quadratic(const float& a, const float& b, const float& c, f
 
     return true;
 }
+
+template<typename A, typename B>
+inline std::unique_ptr<B> static_unique_ptr_cast(std::unique_ptr<A>&& p)
+{
+    B* const d = static_cast<B*>(p.release());
+
+    return std::unique_ptr<B>(d);
+}
+
+template<typename A, typename B>
+inline std::unique_ptr<B> dynamic_unique_ptr_cast(std::unique_ptr<A>&& p)
+{
+    if (B* result = dynamic_cast<B*>(p.get()))
+    {
+        p.release();
+
+        return std::unique_ptr<B>(result);
+    }
+    else
+        return std::unique_ptr<B>(nullptr);
+}
+
