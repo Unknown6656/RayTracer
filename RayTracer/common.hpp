67d235c01b1440cd610af8a485e14087619a1343
unknown6656 <unknown84@gmx.de>
2021-05-24 13:13:53 +0200

    added more common code

--- RayTracer/common.hpp
+++ RayTracer/common.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#define _USE_MATH_DEFINES
+
 #include <cmath>
 #include <math.h>
 #include <sstream>
@@ -20,8 +22,20 @@
 #define EPSILON 1e-6
 #define INFINITY 1e7
 
+#define RAD2DEG(x) ((x) * 57.2957795131f)
+#define DEG2RAD(x) ((x) * .01745329251f)
+#define ROT_45 DEG2RAD(45.f)
+#define ROT_90 DEG2RAD(90.f)
+#define ROT_135 DEG2RAD(135.f)
+#define ROT_180 DEG2RAD(180.f)
+#define ROT_270 DEG2RAD(270.f)
+
+#define SIGN(x) ((x) > 0 ? 1 : (x) < 0 ? -1 : 0)
+
 #define OSTREAM_OPERATOR(x) friend inline std::ostream& operator<<(std::ostream& os, const x& value) { os << value.to_string(); return os; }
 #define CPP_IS_FUCKING_RETARDED(x) inline x& operator =(const x& value) noexcept { return this == &value ? *this : *new(this)x(value); }
+
+#define NAMEOF(x) #x
 
 #define ABSTRACT(ret, name, ...) \
     virtual ret name(__VA_ARGS__) const = 0 \
